48041732421112297e51a97a33d7d23d1afd155a
JohnKihm <john.a.kihm@gmail.com>
2024-04-23 23:31:11 -0500

    added functionality to form.js to create and store a blog post object based on form inputs

---
+++ assets/js/form.js
@@ -0,0 +1,39 @@
+const nameInput = document.querySelector('#user-name');
+const titleInput = document.querySelector('#title');
+const contentInput = document.querySelector('#content');
+const submitButton = document.querySelector('#submit');
+
+const blogPosts = [];
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const name = nameInput.value;
+    const title = titleInput.value;
+    const content = contentInput.value;
+    if (!name || !title || !content) {
+        window.alert('Form cannot be left blank');
+        return;
+    }
+    const newPost = {
+        author: name,
+        title: title,
+        content: content
+    };
+    blogPosts.push(newPost);
+    localStorage.setItem('blogPosts', JSON.stringify(blogPosts));
+}
+
+function loadStorage() {
+    const storedPosts = JSON.parse(localStorage.getItem('blogPosts'));
+    if (!storedPosts) {
+        return;
+    }
+    for (post of storedPosts) {
+        blogPosts.push(post);
+    }
+}
+
+loadStorage();
+console.log(blogPosts);
+
+submitButton.addEventListener('click', handleSubmit);
