6765642e61b0049c78b22ff4df03cdf78c9faead
JohnKihm <john.a.kihm@gmail.com>
2024-04-24 00:24:13 -0500

    added functionality to blog.js to load blog posts from localStorage and add them to the page

---
+++ assets/js/blog.js
@@ -0,0 +1,28 @@
+const mainEl = document.querySelector('main');
+
+function buildBlog() {
+    const storedPosts = JSON.parse(localStorage.getItem('blogPosts'));
+    if (!storedPosts) {
+        placeHolderText = document.createElement('h1');
+        placeHolderText.setAttribute('class', 'place-holder')
+        placeHolderText.textContent = 'Nothing to see here!'
+        mainEl.appendChild(placeHolderText);
+        return;
+   }
+   for (post of storedPosts) {
+    const postEl = document.createElement('article');
+    const titleEl = document.createElement('h2');
+    const contentEl = document.createElement('p');
+    const authorEl = document.createElement('h3');
+    postEl.setAttribute('class', 'blog-post');
+    titleEl.textContent = post.title;
+    contentEl.textContent = post.content;
+    authorEl.textContent = `Posted by: ${post.author}`;
+    postEl.appendChild(titleEl);
+    postEl.appendChild(contentEl);
+    postEl.appendChild(authorEl);
+    mainEl.appendChild(postEl);
+   }
+}
+
+buildBlog();
